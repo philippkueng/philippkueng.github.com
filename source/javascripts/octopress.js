e538b3e75f85a7d6fc6ac1d9d74848c223f2918a
philippkueng <git@philippkueng.ch>
2012-01-10 10:46:49 +0100

    changed index.html layout

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -13,30 +13,30 @@ function getNav() {
   });
 }
 
-function addSidebarToggler() {
-  if(!$('body').hasClass('sidebar-footer')) {
-    $('#content').append('<span class="toggle-sidebar"></span>');
-    $('.toggle-sidebar').bind('click', function(e) {
-      e.preventDefault();
-      if ($('body').hasClass('collapse-sidebar')) {
-        $('body').removeClass('collapse-sidebar');
-      } else {
-        $('body').addClass('collapse-sidebar');
-      }
-    });
-  }
-  var sections = $('aside.sidebar > section');
-  if (sections.length > 1) {
-    sections.each(function(section, index){
-      if ((sections.length >= 3) && index % 3 === 0) {
-        $(section).addClass("first");
-      }
-      var count = ((index +1) % 2) ? "odd" : "even";
-      $(section).addClass(count);
-    });
-  }
-  if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
-}
+// function addSidebarToggler() {
+//   if(!$('body').hasClass('sidebar-footer')) {
+//     $('#content').append('<span class="toggle-sidebar"></span>');
+//     $('.toggle-sidebar').bind('click', function(e) {
+//       e.preventDefault();
+//       if ($('body').hasClass('collapse-sidebar')) {
+//         $('body').removeClass('collapse-sidebar');
+//       } else {
+//         $('body').addClass('collapse-sidebar');
+//       }
+//     });
+//   }
+//   var sections = $('aside.sidebar > section');
+//   if (sections.length > 1) {
+//     sections.each(function(section, index){
+//       if ((sections.length >= 3) && index % 3 === 0) {
+//         $(section).addClass("first");
+//       }
+//       var count = ((index +1) % 2) ? "odd" : "even";
+//       $(section).addClass(count);
+//     });
+//   }
+//   if (sections.length >= 3){ $('aside.sidebar').addClass('thirds'); }
+// }
 
 function testFeatures() {
   var features = ['maskImage'];
